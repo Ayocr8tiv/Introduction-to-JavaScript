7e636a9af5c0efa62b4c87e5dc16c9f07fa9f22f
Ayo <ayocre8tiv@gmail.com>
2024-12-10 12:21:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+
+// Variable Initialization
+
+var fullName = "Ayokunle Oloidi"
+var married = false
+var numberOfChildren = 3
+var disability = null
+var accountNumber = undefined
+
+
+// Console.log
+
+console.log(fullName);
+console.log(numberOfChildren)
+console.log(20+20)
+console.log("Hello World")
+
+
+// Strings operators
+
+var sentence = 'Am going to be a very good software developer'
+console.log(sentence.length)
+console.log(sentence.toUpperCase())
+console.log(sentence.toLowerCase())
+
+// Concatenation
+
+var favouriteTeam ="Manchester United"
+var favouritePlayer = 'Adam Diallo'
+var ability = "Very Good"
+var sentence2 = "My favourite player is " + favouritePlayer + " he plays for " + favouriteTeam + " his " + ability
+console.log(sentence2)
+
+// Arithmetic operators
+
+var x = 5
+var y = 3
+var z = 7
+console.log(z-x)
+console.log(x+y)
+console.log(y*x)
+console.log(z/x)
+console.log(z%y)
+console.log(z**x)
+
+// Math operators
+
+console.log(Math.sqrt(144))
+console.log(Math.pow(16,8))
+console.log(Math.round(4.6))
+console.log(Math.round(4.4))
+console.log(Math.floor(4.6))
+console.log(Math.ceil(4.4))
+
+// Assignment Operators
+
+var num1 = 25
+var num2 = 35
+var num3 = 15
+
+num1 += num3
+console.log(num1)
+
+num2 -= num3
+console.log(num2)
+
+num3 -= num2
+console.log(num3)
+
+
+// Comparison operators
+
+console.log(5==5)
+console.log(5==6)
+console.log(5=='5')
+
+console.log(7===7)
+console.log(7==='7')
+console.log(5!=6)
+console.log(5!='5')
+console.log(5!=='5')
+console.log(100>17)
+console.log(18<=18)
